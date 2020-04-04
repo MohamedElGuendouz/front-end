4d0631ea3967c45780a3e9dd096dc616944f0a49
Mohamed El Guendouz <mohamed.elguendouz88100@gmail.com>
2020-04-04 17:46:59 +0200

    update server.js and package

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+//Install express server
+const express = require('express');
+const path = require('path');
 
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, '/src/index.html'));
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/front-end'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/front-end/index.html'));
 });
 
-app.use(express.static(__dirname)); // set static files location, in this case the route, add a file name if not
-app.listen(process.env.PORT || 3000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
