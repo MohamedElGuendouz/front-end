c31d27e53f766a24b43a3d3ce84c7eb34f20d261
Mohamed El Guendouz <mohamed.elguendouz88100@gmail.com>
2020-04-04 14:32:37 +0200

    updates deployment in heroku

--- server.js
+++ server.js
@@ -1,33 +1,14 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
+const express = require('express');
+const path = require('path');
+const app = express();
 
-var CONTACTS_COLLECTION = "contacts";
+// Serve static files....
+app.use(express.static(__dirname + '/dist/data-sciences-tools'));
 
-var app = express();
-app.use(bodyParser.json());
-
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/data-sciences-tools/index.html'));
 });
 
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
