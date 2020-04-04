abadb7c0957abead1542e5003b07fb7e7f02c80b
Mohamed El Guendouz <mohamed.elguendouz88100@gmail.com>
2020-04-04 14:52:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,5 @@ const app = express();
 // Serve static files....
 app.use(express.static(__dirname + '/dist/front-end'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/front-end/index.html'));
-});
-
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
