31b7a7f050f124e4b97e67f0e8456c4e90cf596b
Mohamed El Guendouz <mohamed.elguendouz88100@gmail.com>
2020-04-04 17:38:10 +0200

    update routing

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require('express');  
-const app = express();
-app.use(express.static(__dirname + '/dist'));
-app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/index.html');
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, '/src/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+
+app.use(express.static(__dirname)); // set static files location, in this case the route, add a file name if not
+app.listen(process.env.PORT || 3000);
