7484996ba48699f32e1daa75cbf5c253a910e8ff
Mohamed El Guendouz <mohamed.elguendouz88100@gmail.com>
2020-04-04 16:51:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const ngApp = express();
+
 ngApp.use(express.static('./dist/front-end'));
+
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/front-end/index.html'));
+    response.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 ngApp.listen(process.env.PORT || 8080);
