6fdb93a9a9981bb2ca5fed44c0bd3f6df59c227e
Mohamed El Guendouz <mohamed.elguendouz88100@gmail.com>
2020-04-04 18:46:13 +0200

    update for routing

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 //Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/front-end'));
+app.use(express.static(__dirname + '/front-end'));
+
+app.get('/*', function(req,res) {
+  res.sendFile(__dirname+ '/front-end/src/index.html');
+});
 
 // default Heroku port
 app.listen(process.env.PORT || 8080);
