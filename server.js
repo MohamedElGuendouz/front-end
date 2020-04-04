a82f527e70b813de6bc9f094c29c1db135a776b8
Mohamed El Guendouz <mohamed.elguendouz88100@gmail.com>
2020-04-04 15:00:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,5 +5,9 @@ const app = express();
 // Serve static files....
 app.use(express.static(__dirname + '/dist/front-end'));
 
+app.get('*', (req, res) => {
+    res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+});
+
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
