2b0b2e047d846c56100f9e9a1b0bf34a85a6cc39
Mohamed El Guendouz <mohamed.elguendouz88100@gmail.com>
2020-04-04 15:47:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-
-//Install express server
 const express = require('express');
-const path = require('path');
 const app = express();
-// Serve only the static files form the dist directory
-app.use(express.static('./front-end'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname,'/front-end/index.html'));
+const path = require('path');
+
+app.use(express.static(__dirname+'/dist'));
+
+app.listen(process.env.PORT||8080);
+
+
+//Path Location Strategy
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+console.log('Console Listening');
